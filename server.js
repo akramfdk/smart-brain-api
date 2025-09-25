73a13e3fc01031a3bcd9ceb3e54b97c6852c109c
akramfdk <akramjeet.singh@gmail.com>
2025-09-25 11:19:45 +1000

    installed knex to connect  postgres database, updated register, profile and image endpoints

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import cors from 'cors';
+import knex from 'knex';
 
 const app = express()
 
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        port: 5432,
+        user: 'postgres',
+        password: 'test',
+        database: 'smart-brain',
+    },
+});
+
+// console.log(db.select('*').from('users'));
+// db.select('*').from('users').then(data => console.log(data));
+
+app.use(cors());
 app.use(express.json());
 
 const database = {
@@ -32,7 +49,8 @@ app.get("/", (req, res) => {
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email && 
     req.body.password === database.users[0].password){
-        res.json("success");
+        // res.json("success");
+        res.json(database.users[0]);
     } else {
         res.status(400).json("Error logging in");
     }
@@ -43,53 +61,45 @@ app.post('/register', (req, res) => {
 
     const {email, name, password} = req.body;
 
-    database.users.push({
-        id: '125',
-        name: name,
+    db('users')
+        .returning('*')
+        .insert({
         email: email,
-        password: password,
-        entries: 0,
+        name: name,
         joined: new Date()
     })
-
-    res.json(database.users.at(-1));
-    // console.log(req.body);
+        .then(user => {
+            res.json(user[0]);
+        })
+        .catch(err => res.status(400).json('unable to register'));
 })
 
 app.get('/profile/:id', (req, res) => {
     const {id} = req.params;
 
-    const user = database.users.find(user => user.id === id);
-
-    if (user){
-        res.json(user);
-    } else {
-        res.status(400).json("not found");
-    }
+    db.select('*').from('users').where({id})
+        .then(user => {
+            if (user.length){
+                res.json(user[0]);
+            } else {
+                res.status(400).json("not found")
+            }
+        })
+        .catch(err => res.status(400).json("error getting user"));
 })
 
 app.put('/image', (req, res) => {
     const {id} = req.body;
 
-    const user = database.users.find(user => user.id === id);
-
-    if (user){
-        user.entries++
-        res.json(user.entries);
-    } else {
-        res.status(400).json("not found");
-    }
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries)
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 })
 
 app.listen(3000, () => {
     console.log("app is running on port 3000");
 });
-
-
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT = updated user
-*/
